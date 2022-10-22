6ba39d69f5e72c8d4e9f4038eb31d134ae164b5a
Flavia Tani <98674255+flaviatani@users.noreply.github.com>
2022-10-21 23:16:56 -0300

    Update app.js

--- Menu/app.js
+++ Menu/app.js
@@ -3,11 +3,43 @@
 let Menulateral = document.querySelector(".Menulateral");
 let closeBtn = document.querySelector("#btn");
 let inicioBtn = document.querySelector(".bx-grid-alt");
+let searchBtn = document.querySelector(".bx-search");
+let userBtn = document.querySelector(".bx-user");
+let cartBtn = document.querySelector(".bx-cart-alt");
+let chatBtn = document.querySelector(".bx-chat");
+let folderBtn = document.querySelector(".bx-folder");
+let icogBtn = document.querySelector(".bx-cog");
 
-closeBtn.addEventListener("click", ()=>{ // O menu abre ao clicar no icone de Menu.
+// O menu abre ao clicar nos botoes do menu.
+
+closeBtn.addEventListener("click", ()=>{ 
   Menulateral.classList.toggle("open");
  });
 
-inicioBtn.addEventListener("click", ()=>{ // O menu abre ao clicar no botao de inicio.
+inicioBtn.addEventListener("click", ()=>{ 
   Menulateral.classList.toggle("open");
+});
+
+searchBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
+});
+
+userBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
+});
+
+cartBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
+});
+
+chatBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
+});  
+
+folderBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
+});
+
+icogBtn.addEventListener("click", ()=>{ 
+    Menulateral.classList.toggle("open");
 });
